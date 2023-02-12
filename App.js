b921cd9b37e7e8c64aa369962a8a86bf527069af
Ayman717 <87248906+Ayman717@users.noreply.github.com>
2023-02-13 01:23:01 +0200

    ToDo-App

---
+++ App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './pages/Home'
+
+export default function App() {
+  
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
